e681352a70c93f2e099f724930a874ea6c5530ec
Ionut Cotoi <ionut@devicehub.net>
2016-11-07 20:33:50 +0200

    code refactory & more boilerplate

--- itbp-gprs.py
+++ itbp-gprs.py
@@ -10,56 +10,74 @@ RESET = 18  # DON'T NEEDED!!! ==> RESET PIN IT IS NC...cross compatibility with 
 STATUS = 12
 
 
-def poweron():
-    if not (getModemState()):
+def modem_status():
+    return GPIO.input(STATUS)
+
+
+def modem_power_on():
+    if not modem_status():
         print("try to wake h-nanoGSM")
         GPIO.output(POWER, GPIO.LOW)
         sleep(1)
         GPIO.output(POWER, GPIO.HIGH)
     sleep(5)
     # if GPIO.input(STATUS):
-    if (getModemState()):
+    if modem_status():
         print("h-nanoGSM is up")
     else:
         print("failure powering h-nanoGSM")
         exit(100)
 
 
-def poweroff():
+def modem_power_off():
     # if GPIO.input(STATUS):
-    if (getModemState()):
+    if modem_status():
         print("try to shutdown h-nanoGSM")
         GPIO.output(POWER, GPIO.LOW)
         sleep(1)
         GPIO.output(POWER, GPIO.HIGH)
     sleep(8)
     # if not GPIO.input(STATUS):
-    if not (getModemState()):
+    if not modem_status():
         print("h-nanoGSM is down")
     else:
         print("failure powering off h-nanoGSM")
         exit(100)
 
 
-def restartModem():
-    poweroff()
+def modem_restart():
+    modem_power_off()
     sleep(3)
-    poweron()
+    modem_power_on()
 
     def getModemState():
         return GPIO.input(STATUS)
 
 
-def hwControlSetup():
+def modem_hw_control_setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
     try:
         GPIO.setup(STATUS, GPIO.IN)
         GPIO.setup(POWER, GPIO.OUT, initial=GPIO.HIGH)
     # GPIO.setup(RESET, GPIO.OUT, initial=GPIO.HIGH)#RESET PIN IT IS NC...cross compatibility with c-uGSM
-    except:
+    except Exception as e:
+        print str(e)
         GPIO.cleanup()  # free GPIO
         GPIO.setup(STATUS, GPIO.IN)
         GPIO.setup(POWER, GPIO.OUT, initial=GPIO.HIGH)
     # GPIO.setup(RESET, GPIO.OUT, initial=GPIO.HIGH)#RESET PIN IT IS NC...cross compatibility with c-uGSM
     GPIO.setwarnings(True)
+
+
+def modem_hw_control_release():
+    GPIO.cleanup()  # free GPIO
+
+
+def modem_supervisord():
+    while True:
+        sleep(1)
+
+if __name__ == "__main__":
+    modem_hw_control_setup()
+    modem_supervisord()
