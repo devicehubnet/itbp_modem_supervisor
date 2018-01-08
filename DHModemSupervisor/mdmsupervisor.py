e7ec3dac4774df3cd3fff36fff61ed539e0620dc
Ionut Cotoi <ionut@devicehub.net>
2018-01-08 18:37:08 +0200

    Added BBB GPIO and Modem classs

--- DHModemSupervisor/mdmsupervisor.py
+++ DHModemSupervisor/mdmsupervisor.py
@@ -1,11 +1,49 @@
 from transitions import Machine
 from .modem import Modem
+from time import sleep
+import ConfigParser
+import os
+from subprocess import Popen
 
 
 class ModemSupervisor(Machine):
+    PIN_POWER = 16
+    PIN_RESET = 18
+    PIN_STATUS = 12
+
+    INI_FILE = '/etc/devicehub/itbp-gprs.ini'
+
+    APN = 'internet'
+    ISP = 'isp'
+
+    AUTO_CONNECT = False
+
+    PPP_CONNECTED = False
+    NET_CONNECTED = False
+
     states = ['internet_disconnected', 'internet_connected', 'modem_reset']
+    modem = None
 
     def __init__(self):
+        # Try to open the config file /etc/itbp-gprs.ini and populate settings
+        try:
+            config = ConfigParser.ConfigParser()
+            config.read(self.INI_FILE)
+
+            self.PIN_POWER  = config.getint('Modem', 'gpio_power')
+            self.PIN_RESET  = config.getint('Modem', 'gpio_reset')
+            self.PIN_STATUS = config.getint('Modem', 'gpio_status')
+
+            # self.APN = config.get('Connection', 'apn', )
+            self.ISP = config.get('Connection', 'isp')
+            self.AUTO_CONNECT = config.getboolean('Connection', 'auto_connect')
+
+            self.log("ISP: " + self.ISP)
+            self.log("APN: " + self.APN)
+            self.log("AUTO_CONNECT: " + str(self.AUTO_CONNECT))
+        except Exception as e:
+            self.log("config file init EXC: " + str(e))
+
         Machine.__init__(self, states=self.states, initial='internet_disconnected')
         self.add_transition('disconnecting', 'internet_connected', 'internet_disconnected')
         self.add_transition('connecting', 'internet_disconnected', 'internet_connected')
@@ -15,7 +53,51 @@ class ModemSupervisor(Machine):
         self.setup_platform()
 
     def setup_platform(self):
+        self.modem = Modem(self.PIN_POWER, self.PIN_RESET, self.PIN_STATUS)
 
+    def ppp_connect(self):
+        # os.system("pon {ISP}".format(ISP=self.ISP))
+        os.system("pppd call {ISP}".format(ISP=self.ISP))
+
+    def ppp_disconnect(self):
+        # os.system("poff {ISP}".format(ISP=self.ISP))
+        os.system("killall -9 pppd")
+
+    def ppp_status(self):
+        try:
+            for line in os.popen("/sbin/ip link show "):
+                if 'ppp' in line:
+                    return True
+        except Exception as e:
+            self.log(e)
+
+        return False
+
+    def net_status(self):
+        p = Popen(["ping", "-c1", "openfleet.opendevlabs.com"])
+        output = p.communicate()[0]
+        if p.returncode == 0:
+            self.log(output)
+            return True
+        else:
+            return False
+
+    def supervisord(self):
+        self.log("Starting supervisor loop")
+        while True:
+            # do we have ppp up?
+            ppp_state = self.ppp_status()
+            net_state = self.net_status()
+            print "PPP Status:", "UP" if ppp_state else "DOWN"
+            print "NET Status:", "UP" if net_state else "DOWN"
+            if net_state is False:
+                print "Attempting to start PPP connection..."
+                self.ppp_disconnect()
+                self.modem_power_off()
+                sleep(1)
+                self.modem_power_on()
+                self.ppp_connect()
+            sleep(60 * 2)
 
     def run(self):
         while True:
--- DHModemSupervisor/modem.py
+++ DHModemSupervisor/modem.py
@@ -1,56 +1,9 @@
-import os
+from time import sleep
 import sys
 try:
-     import RPi.GPIO as GPIO
+    import RPi.GPIO as GPIO
 except ImportError:
-     class GPIO(object):
-          BOARD=0
-          IN="in"
-          OUT="out"
-          LOW=0
-          HIGH=1
-          WARNING=True
-
-          def __init__(self):
-               pass
-
-          @classmethod
-          def setmode(cls, mode):
-               pass
-          @classmethod
-          def input(cls, pin):
-               return 1
-
-          @classmethod
-          def output(cls, pin, state):
-               gpio=pin
-               cmd = "echo {VAL} > /sys/class/gpio/gpio{GPIO}/value".format(VAL=state, GPIO=gpio)
-               print cmd
-               os.system(cmd)
-
-          @classmethod
-          def setup(cls, pin, direction, initial=None):
-               gpio=pin
-               cmd = "echo {GPIO} >/sys/class/gpio/export".format(GPIO=gpio)
-               print cmd
-               os.system(cmd)
-               sleep(1)
-               cmd = "echo {DIR} > /sys/class/gpio/gpio{GPIO}/direction".format(DIR=direction, GPIO=gpio)
-               print cmd
-               os.system(cmd)
-
-          @classmethod
-          def cleanup(cls):
-               pass
-
-          @classmethod
-          def setwarnings(cls, enable):
-               cls.WARNING=enable
-
-
-from time import sleep
-import ConfigParser
-from subprocess import Popen
+    from .bbbgpio import GPIO
 
 
 class Modem(object):
@@ -58,37 +11,11 @@ class Modem(object):
     PIN_RESET = 18
     PIN_STATUS = 12
 
-    INI_FILE = '/etc/devicehub/itbp-gprs.ini'
-
-    APN = 'internet'
-    ISP = 'isp'
-
-    AUTO_CONNECT = False
-
-    PPP_CONNECTED = False
-    NET_CONNECTED = False
+    TIMEOUT_POWER_ON = 8
+    TIMEOUT_POWER_OFF_MAX = 100
 
     def __init__(self):
-        # Try to open the config file /etc/itbp-gprs.ini and populate settings
-        try:
-            config = ConfigParser.ConfigParser()
-            config.read(self.INI_FILE)
-
-            self.PIN_POWER  = config.getint('Modem', 'gpio_power')
-            self.PIN_RESET  = config.getint('Modem', 'gpio_reset')
-            self.PIN_STATUS = config.getint('Modem', 'gpio_status')
-
-            # self.APN = config.get('Connection', 'apn', )
-            self.ISP = config.get('Connection', 'isp')
-            self.AUTO_CONNECT = config.getboolean('Connection', 'auto_connect')
-
-            self.log("ISP: " + self.ISP)
-            self.log("APN: " + self.APN)
-            self.log("AUTO_CONNECT: " + str(self.AUTO_CONNECT))
-        except Exception as e:
-            self.log("config file init EXC: " + str(e))
-
-        self.modem_hw_control_setup()
+        self.modem_hw_control_setup(pin_power=16, pin_reset=18, pin_status=12)
 
     def __del__(self):
         self.modem_hw_control_release()
@@ -114,11 +41,22 @@ class Modem(object):
 
     def modem_power_off(self):
         if self.modem_status():
+            delay = 0
             print("itbp modem: try to shutdown h-nanoGSM")
             GPIO.output(self.PIN_POWER, GPIO.LOW)
             sleep(1)
             GPIO.output(self.PIN_POWER, GPIO.HIGH)
-            sleep(8)
+
+            sys.stdout.write("itbp modem: wait.")
+            sys.stdout.flush()
+
+            while self.modem_status() and delay < self.TIMEOUT_POWER_OFF_MAX:
+               sys.stdout.write(".")
+               sys.stdout.flush()
+               sleep(1)
+               delay += 1
+
+            print "done"
 
         if not self.modem_status():
             print("itbp modem: h-nanoGSM is down")
@@ -145,49 +83,3 @@ class Modem(object):
 
     def modem_hw_control_release(self):
         GPIO.cleanup()  # free GPIO
-
-    def ppp_connect(self):
-        # os.system("pon {ISP}".format(ISP=self.ISP))
-        os.system("pppd call {ISP}".format(ISP=self.ISP))
-
-    def ppp_disconnect(self):
-        # os.system("poff {ISP}".format(ISP=self.ISP))
-        os.system("killall -9 pppd")
-
-    def ppp_status(self):
-        try:
-            for line in os.popen("/sbin/ip link show "):
-                if 'ppp' in line:
-                    return True
-        except Exception as e:
-            self.log(e)
-
-        return False
-
-    def net_status(self):
-        p = Popen(["ping", "-c1", "openfleet.opendevlabs.com"])
-        output = p.communicate()[0]
-        if p.returncode == 0:
-            self.log(output)
-            return True
-        else:
-            return False
-
-    def supervisord(self):
-        self.log("Starting supervisor loop")
-        while True:
-            # do we have ppp up?
-            ppp_state = self.ppp_status()
-            net_state = self.net_status()
-            print "PPP Status:", "UP" if ppp_state else "DOWN"
-            print "NET Status:", "UP" if net_state else "DOWN"
-            if net_state is False:
-                print "Attempting to start PPP connection..."
-                self.ppp_disconnect()
-                self.modem_power_off()
-                sleep(1)
-                self.modem_power_on()
-                self.ppp_connect()
-            sleep(60 * 2)
-
-
